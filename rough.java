8a8f0f18e9f0a105b54cd902620807cc32f0eaa7
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-02-16 16:58:00 +0530

    adding some very important questions

--- rough.java
+++ rough.java
@@ -52,7 +52,32 @@
 //         System.out.println("The reversed string is: " + StringTwo);
 //     }
 // }
-
 // Remove special characters from the string "take12% *&u ^$#forward"
 
- 
+// import java.util.Scanner;
+
+// public class rough {
+
+    // Method to reverse a string
+    // static String value(String str) {
+    //     StringBuilder newString = new StringBuilder(str);
+    //     return newString.reverse().toString();
+    // }
+//     static String value(String str)
+//     {
+//         String reversedString = "";
+//         for(int i=str.length() - 1; i>=0; i--)
+//         {
+//             reversedString += str.charAt(i);
+//         }
+//         return reversedString;
+//     }
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter your string: ");
+//         String input = scanner.nextLine(); 
+//         System.out.println("Reversed string is: " + value(input));
+//         scanner.close();
+//     }
+// }
+
