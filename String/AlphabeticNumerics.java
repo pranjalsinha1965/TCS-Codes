98be77597e2b3b8ea730ea31fa9b376f884f6fb5
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-02-25 23:24:22 +0530

    Do exam questions daily

---
+++ String/AlphabeticNumerics.java
@@ -0,0 +1,18 @@
+package String;
+class AlphabeticNumerics {
+    public static void main(String[] args) {
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        char maxChar = ' '; 
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if ((currentChar >= 'A' && currentChar <= 'Z') || (currentChar >= 'a' && currentChar <= 'z')) {
+                if (currentChar > maxChar) {
+                    maxChar = currentChar;
+                }
+            }
+        }
+
+        System.out.println("Highest alphabetic character: " + maxChar);
+    }
+}
