efcff343f2394241e2590e9c1e7da06fc63abfdd
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-03-01 03:00:23 +0530

    Daily practice sheets

--- Level-2/TestMedian.java
+++ Level-2/TestMedian.java
@@ -12,7 +12,6 @@ class TestMedian {
             System.out.print(arr[(n / 2)]);
         }
     }
-
     public static void main(String[] args) {
         int[] arr = {4, 7, 1, 2, 5, 6};
         int n = arr.length; 
