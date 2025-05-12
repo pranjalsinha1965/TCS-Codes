77e1471f6cdce578dac652a20d0c0cc3d3ee8121
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-05-12 16:55:21 +0530

    Added and rectified questions for practice

---
+++ rough.java
@@ -0,0 +1,19 @@
+public class rough{
+    public static void main(String args[])
+    {
+        int n = 5;
+        if(remainder(n) == 0)
+        {
+            System.out.println(n + " is even ");
+
+        }
+        else
+        {
+            System.out.println(n + " is odd ");
+        }
+    }
+    public static int remainder(int n)
+    {
+        return (n % 2);
+    }
+}
