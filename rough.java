98ab29258f8df901ebf58c4f64bf3ce32793f974
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-10-21 02:04:13 +0530

    Code daily

--- rough.java
+++ rough.java
@@ -1,2 +1,8 @@
 // {10,9,14,8,20,48,16,9,0} insert the 40 at the beginning only. 
 // Using DP create Fibonacci series upto 5th term.Please make one iterative solution to print and then generate it using DP using.
+// Find the highest alphabetic character in the given string. 
+// Print the returned ASCII value character. 
+// Provide a palindrome checker function using driver code.  
+// Make a java function to remove brackets. 
+// Remove special characters from the given string 
+
