78a39cb036bfdf521b05c9236442b47ee2f0bb92
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-10-29 17:39:17 +0530

    Keep solving the codes daily

--- rough.java
+++ rough.java
@@ -9,3 +9,37 @@
 // "(((a-b))+c)"
 // "take12% *&u ^$#forward" Remove special characters from the string "take12% *&u ^$#forward
 
+// import java.util.Stack;
+// public class rough {
+//     public static void main(String args[])
+//     {
+//         String str = "HELLO";
+//         char[] reverseString = new char[str.length()];
+//         Stack<Character> stack = new Stack<Character>();
+//         for(int i=0; i<str.length(); i++)
+//         {
+//             stack.push(str.charAt(i));
+//         }
+//         int i=0;
+//         while(!stack.isEmpty())
+//         {
+//             reverseString[i++] = stack.pop();
+//         }
+//         str = new String(reverseString);
+//         System.out.println("The reversed string is: " + str);
+//     }
+// }
+
+// public class ReverseString {
+//     public static void main(String[] args) {
+//         String str = "Hello";
+//         char[] charArray = str.toCharArray();
+//         for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
+//             char temp = charArray[i];
+//             charArray[i] = charArray[j];
+//             charArray[j] = temp;
+//         }
+//         String reversed = new String(charArray);
+//         System.out.println("Reversed (For Loop): " + reversed);
+//     }
+// }
