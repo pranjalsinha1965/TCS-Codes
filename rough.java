8947da62e6b980626d48728286e354503d8b8b26
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-11-11 16:56:44 +0530

    Keep adding the files and codes for new questions

--- rough.java
+++ rough.java
@@ -48,25 +48,4 @@
 // Insertion Sort: [12, 11, 13, 5, 6]
 // Quick Sort: [4, 6, 2, 5, 7, 9, 1, 3]
 
-public class rough{
-    static private void MinMax(int n)
-    {
-        int d;
-        int mini = Integer.MAX_VALUE;
-        int maxi = Integer.MIN_VALUE;
-        while(n != 0)
-        {
-            d = n % 10;
-            mini = Math.min(mini, d);
-            maxi = Math.max(maxi, d);
-            n = n / 10;
-        }
-        System.out.println("The minimum number is: " + mini);
-        System.out.println("The maximum number is: " + maxi);
-    }
-    public static void main(String args[])
-    {
-        int n = 4726;
-        MinMax(n);
-    }
-}
+
