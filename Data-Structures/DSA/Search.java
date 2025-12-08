a9064289ccff4eef8bc3d568d1267054fb232735
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-12-08 17:04:53 +0530

    Add data-structures

---
+++ Data-Structures/DSA/Search.java
@@ -0,0 +1,15 @@
+package DSA;
+// complete the function as in your homework only !
+public class Search {
+    public static void main(String args[])
+    {
+
+    }
+    // public static int linearSearch(int[] nums, int target){
+
+    // }
+    // public static int binarySearch(int[] nums, int target)
+    // {
+
+    // }
+}
