9e820c6d5d0f22277aa5129be73ef388cc8d01ea
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-12-11 13:53:55 +0530

    Add submodules

--- Data-Structures/DSA/src/queue/CircularQueue.java
+++ Data-Structures/DSA/src/queue/CircularQueue.java
@@ -1,41 +1,28 @@
 package DSA.src.queue;
-
 public class CircularQueue {
-
 	public static void main(String[] args)   {
-		
 		Queue queue = new Queue();
-		
 		queue.enqueue(10);
 		queue.enqueue(20);
 		queue.enqueue(5);
 		queue.enqueue(99);
-		
 		System.out.println(queue.dequeue());
 		queue.enqueue(12);
 		queue.enqueue(32);
-		
 		System.out.println("Peek "+queue.peek());
-		
 		System.out.println(queue.dequeue());
 		System.out.println(queue.dequeue());
 		System.out.println(queue.dequeue());
 		System.out.println(queue.dequeue());
 		System.out.println(queue.dequeue());
-		
 //		queue.show();
-
 	}
 }
-
-
-class Queue{
-	
+class Queue{	
 	private int front = 0;
 	private int rear = -1;
 	private int size = 0;
 	private int[] arr = new int[4];
-	
 	public void enqueue(int data)
 	{
 		if(!isFull())
@@ -48,43 +35,31 @@ class Queue{
 			System.out.println("Queue if full");
 		}
 	}
-	
-	
 	public int dequeue()
 	{
 		if(isEmpty()) 
 		{
 			throw new RuntimeException("Queue is Empty");
-		}
-		
+		}	
 		int data = arr[front];
 		front = (front+ + 1) % 4;
 		size--;
 		return data;
 	}
-	
-	
 	public int peek()
 	{
-		if(isEmpty()) 
-		
+		if(isEmpty()) 	
 			throw new RuntimeException("Queue is Empty");
 		return arr[front];
 	}
-	
-	
 	public boolean isFull()
 	{
 		return size ==4;
 	}
-	
-	
 	public boolean isEmpty()
 	{
 		return size ==0;
 	}
-	
-	
 	public void show()
 	{
 		for(int i=front; i<size; i++)
@@ -93,5 +68,4 @@ class Queue{
 		}
 		System.out.println();
 	}
-	
 }
