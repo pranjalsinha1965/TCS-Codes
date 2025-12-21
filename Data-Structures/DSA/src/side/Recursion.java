32d27603e3dd5d92b74c14c8e9131b0350f4496f
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-12-22 00:54:45 +0530

    Practice Daily

--- Data-Structures/DSA/src/side/Recursion.java
+++ Data-Structures/DSA/src/side/Recursion.java
@@ -1,32 +1,29 @@
 package DSA.src.side;
+
 public class Recursion {
 
-	public static void main(String[] args) {
-		
-//		f1(10);
-//	}
-//	
-//	public static void f1(int i)
-//	{
-//		System.out.println(i);
-//		if(i>0)
-//			f1(i-1);
-//	}
-	
-	
-	int result = fact(10);
-	System.out.println(result);
-	}
-	
-	public static int fact(int i)
-	{
-		if(i!=0)
-			return i * fact(i-1);
-		
-		return 1;
-	}
-	
-	// 5! = 5 * 4 * 3 * 2 * 1
-	// 5! = 5 * 4!
+    public static void main(String[] args) {
 
+        int result = fact(10);
+        System.out.println(result);
+
+        // f1(5);  // Uncomment to test printing recursion
+    }
+
+    // Factorial using recursion
+    public static int fact(int i) {
+        if (i == 0)
+            return 1;
+
+        return i * fact(i - 1);
+    }
+
+    // Print numbers using recursion
+    public static void f1(int i) {
+        if (i < 0)
+            return;
+
+        System.out.println(i);
+        f1(i - 1);
+    }
 }
